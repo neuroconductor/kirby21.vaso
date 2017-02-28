e69d0786fa1b93882a5427f020602dc0470c93da
muschellij2 <muschellij2@gmail.com>
2017-02-27 23:28:24 -0500

    adding readme

---
+++ R/download_vaso_data.R
@@ -0,0 +1,16 @@
+#' @title Download VASO Image Filenames
+#'
+#' @description Download the files for the VASO images
+#' @param ... arguments to pass to \code{\link{download_kirby21_data}},
+#' \code{modalities = "VASO"} so it cannot be specified
+#' @return Indicator if the data is downloaded.
+#' 
+#' @examples
+#' download_vaso_data()
+#' @export
+#' @importFrom kirby21.base download_kirby21_data
+download_vaso_data =  function(...) {  
+  x = kirby21.base::download_kirby21_data(modality = "VASO", ...)
+  return(x)
+}
+
