07691e030fe0798044073daca0716a974ae6dc47
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:07:36 -0500

    updated description with doi

--- R/get_vaso_filenames.R
+++ R/get_vaso_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the VASO images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "VASO"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_vaso_filenames()
